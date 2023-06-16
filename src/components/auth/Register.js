754d891645129383b50a4370cb7c9ca9ce1bf0c4
Nicholas Adams <njadams.722@gmail.com>
2023-06-16 12:35:04 -0500

    initial commit

---
+++ src/components/auth/Register.js
@@ -0,0 +1,142 @@
+import React, { useRef, useState } from "react"
+import { Link, useNavigate } from "react-router-dom"
+import { registerUser } from "../../managers/AuthManager"
+import "./Auth.css"
+
+export const Register = () => {
+    const firstName = useRef()
+    const lastName = useRef()
+    const username = useRef()
+    const password = useRef()
+    const isStaff = useRef()
+    const age = useRef()
+    const gradeLevel = useRef()
+    const verifyPassword = useRef()
+    const passwordDialog = useRef()
+    const navigate = useNavigate()
+    const [isChecked, setIsChecked] = useState(false);
+
+    const handleRegister = (e) => {
+        e.preventDefault()
+
+    
+
+        if (password.current.value === verifyPassword.current.value) {
+            const newUser = {
+                "username": username.current.value,
+                "first_name": firstName.current.value,
+                "last_name": lastName.current.value,
+                "password": password.current.value,
+                "is_staff": isChecked,
+            }
+
+            registerUser(newUser)
+                .then(res => {
+                    if ("token" in res) {
+                        localStorage.setItem("cb_token", res.token)
+                        navigate("/")
+                    }
+                })
+        } else {
+            passwordDialog.current.showModal()
+        }
+    }
+
+    const handleCheck = () => {
+        setIsChecked(!isChecked);
+      };
+
+    return (
+        <main style={{ textAlign: "center" }}>
+
+            <dialog className="dialog dialog--password" ref={passwordDialog}>
+                <div>Passwords do not match</div>
+                <button className="button--close" onClick={e => passwordDialog.current.close()}>Close</button>
+            </dialog>
+
+            <form className="form--login" onSubmit={handleRegister}>
+                <h1 className="h3 mb-3 font-weight-normal">Register an account</h1>
+                <div>
+                    <label>
+                        Staff:
+                        <input
+                        type="checkbox"
+                        checked={isChecked}
+                        onChange={handleCheck}
+                        />
+                    </label>
+                    <span>{isChecked ? "Yes" : "No"}</span>
+                </div>
+                <fieldset>
+                    <label htmlFor="firstName"> First Name </label>
+                    <input ref={firstName} type="text" name="firstName" className="form-control" placeholder="First name" required autoFocus />
+                </fieldset>
+                <fieldset>
+                    <label htmlFor="lastName"> Last Name </label>
+                    <input ref={lastName} type="text" name="lastName" className="form-control" placeholder="Last name" required />
+                </fieldset>
+                <fieldset>
+                    <label htmlFor="inputUsername">Username</label>
+                    <input ref={username} type="text" name="username" className="form-control" placeholder="Username" required />
+                </fieldset>
+                <fieldset>
+                    <label htmlFor="inputPassword"> Password </label>
+                    <input ref={password} type="password" name="password" className="form-control" placeholder="Password" required />
+                </fieldset>
+                <fieldset>
+                    <label htmlFor="verifyPassword"> Verify Password </label>
+                    <input ref={verifyPassword} type="password" name="verifyPassword" className="form-control" placeholder="Verify password" required />
+                </fieldset>
+
+
+                {!isChecked && (
+          <>
+            <fieldset>
+              <label htmlFor="age"> Age </label>
+              <input
+                ref={age}
+                type="text"
+                name="age"
+                className="form-control"
+                placeholder="Age"
+                required
+              />
+            </fieldset>
+
+            <fieldset>
+              <label htmlFor="gradeLevel"> Grade Level </label>
+              <select
+                ref={gradeLevel}
+                name="gradeLevel"
+                className="form-control"
+                required
+              >
+                <option value="">Select Grade Level</option>
+                <option value="1st">1st</option>
+                <option value="2nd">2nd</option>
+                <option value="3rd">3rd</option>
+                <option value="4th">4th</option>
+                <option value="5th">5th</option>
+                <option value="6th">6th</option>
+                <option value="7th">7th</option>
+                <option value="8th">8th</option>
+              </select>
+            </fieldset>
+
+        </>
+    )}
+
+
+                <fieldset style={{
+                    textAlign: "center"
+                }}>
+                    <button className="btn btn-1 btn-sep icon-send" type="submit">Register</button>
+                </fieldset>
+            </form>
+            <section className="link--register">
+                Already registered? <Link to="/login">Login</Link>
+            </section>
+        </main>
+    )
+}
+
